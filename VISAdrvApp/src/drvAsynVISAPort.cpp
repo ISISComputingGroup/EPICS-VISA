30580a23863eac7188dc2ed976febd1f116caa9b
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2017-07-19 15:30:45 +0100

    Mods for Linux

--- VISAdrvApp/src/drvAsynVISAPort.cpp
+++ VISAdrvApp/src/drvAsynVISAPort.cpp
@@ -562,7 +562,7 @@ static asynStatus writeIt(void *drvPvt, asynUser *pasynUser,
 			err = viSetAttribute(driver->vi, VI_ATTR_TMO_VALUE, static_cast<int>(driver->timeout * 1000.0));
 		}
 		VI_CHECK_ERROR("set timeout", err);
-	unsigned long actual = 0;
+	ViUInt32 actual = 0;
 	err = viWrite(driver->vi, (ViBuf)data, static_cast<ViUInt32>(numchars), &actual);
 	if ( err == VI_ERROR_TMO )
 	{
@@ -599,7 +599,7 @@ static asynStatus readIt(void *drvPvt, asynUser *pasynUser,
     int reason = 0;
     asynStatus status = asynSuccess;
 	epicsTimeStamp epicsTS1, epicsTS2;
-	unsigned long actual = 0, actualex = 0;
+	ViUInt32 actual = 0, actualex = 0;
 	ViStatus err;
 
     assert(driver);
@@ -762,7 +762,7 @@ static asynStatus readIt(void *drvPvt, asynUser *pasynUser,
 //    }
     *nbytesTransfered = actual;
     /* If there is room add a null byte */
-    if (actual < (int) maxchars)
+    if (actual < maxchars)
         data[actual] = 0;
     else
         reason |= ASYN_EOM_CNT;
